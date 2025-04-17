502cb76d935a6dd3979afe34529b0a3b3107c972
devkentron <himanshu4shukla4l@gmail.com>
2025-04-17 12:09:43 +0530

    changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -60,7 +60,6 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
           </a>
         </div>
         <Button appName="web" className={styles.secondary}>
